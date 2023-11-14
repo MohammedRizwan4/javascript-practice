a57db7badaac1420a7909ee1ce653cc68733a5ca
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2023-11-14 21:45:13 +0530

    let and const completed

---
+++ JS Let and Const/index.js
@@ -0,0 +1,92 @@
+// {
+//     let a = 90;
+// }
+// console.log(a);
+
+// {
+//     const b = 90;
+// }
+// console.log(b)
+
+{
+  var c = 10;
+}
+c;
+
+var abc = 10;
+abc;
+{
+  var abc = 890;
+  abc;
+}
+abc;
+
+let g = 10;
+g;
+{
+  let g = 90;
+  g;
+}
+g;
+
+const h = 90;
+h;
+{
+  const h = 20;
+  h;
+}
+h;
+
+var x = 90;
+x;
+{
+  let x = 20;
+  x;
+}
+x;
+
+let l = 0;
+{
+  let l = 2;
+}
+{
+  let l = 1;
+}
+
+carName;
+carName = "Volvo";
+carName;
+var carName;
+
+// const PI = 3.141592;
+// PI = PI + 10;
+// PI = 10
+
+const cars = ["Volvo", "Saab", "BMW"];
+cars;
+cars[0] = "Maruti";
+cars;
+cars.push("Farari");
+cars;
+
+const cars1 = ["Saab", "Volvo", "BMW"];
+// cars1 = ["Toyota", "Volvo", "Audi"];    // ERROR
+
+const car = {
+  type: "Fiat",
+  model: "500",
+  color: "White",
+};
+
+car;
+
+car.color = "Red";
+car;
+
+car.owner = "Rizwan"
+car
+
+const car1 = {type:"Fiat", model:"500", color:"white"};
+// car1 = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
+
+
