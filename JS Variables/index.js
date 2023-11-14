20944583953e7e3f29075ca3ae7da24f4687a63a
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2023-11-14 21:19:36 +0530

    js variables completed

--- JS Variables/index.js
+++ JS Variables/index.js
@@ -1,1 +1,51 @@
-console.log("Hello world")
+x = 9;
+y = 10;
+
+console.log(x+y)
+
+
+var carName;
+let carName1;
+const carName2 = undefined;
+
+console.log(carName)
+console.log(carName1)
+console.log(carName2)
+
+
+a = 90;
+var a = 78;
+console.log(a)
+
+
+// b = 21;
+// let b = 89;
+// console.log(b)
+
+
+let person = "Mohammed",
+age = 16,
+address = "India";
+
+
+var newName = "Volvo";
+var newName = "Maruti";
+
+// let newName1 = "Kumar";
+// let newName1 = "singh";
+
+let newName2 = "First";
+newName2 = "Second";
+console.log(newName2)
+
+
+let $ = 32;
+let $$ = 90;
+let $$$$$$$ = 100;
+console.log($ + $$ + $$$$$$$)
+
+let _lastName = "Johnson";
+let _x = 2;
+let _100 = 5;
+console.log(_lastName, _x, _100);
+
